580393f53016d53dd183b45078887b18bb8396b6
cht0977 <christophertent@gmx.de>
2017-10-21 16:51:03 +0200

    sí

---
+++ includes/db-config.php
@@ -0,0 +1,11 @@
+<?php
+define("HOST", "localhost");     // Der Host mit dem du dich verbinden willst.
+define("USER", "sec_user");    // Der Datenbank-Benutzername. 
+define("PASSWORD", "nadine");    // Das Datenbank-Passwort. 
+define("DATABASE", "beichris");    // Der Datenbankname.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // NUR FÜR DIE ENTWICKLUNG!!!!
+?>
---
+++ includes/db-connect.php
@@ -0,0 +1,4 @@
+<?php
+include_once 'db-config.php';
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+?>
